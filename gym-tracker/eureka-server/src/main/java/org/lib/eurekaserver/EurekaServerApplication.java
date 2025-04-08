e6671d01b067132a2ae6464fd55c3bc073e21721
Soluckyo <twinki0000@yandex.ru>
2025-04-09 00:07:56 +0300

    добавил пустой auth-service

--- gym-tracker/eureka-server/src/main/java/org/lib/eurekaserver/EurekaServerApplication.java
+++ gym-tracker/eureka-server/src/main/java/org/lib/eurekaserver/EurekaServerApplication.java
@@ -2,9 +2,10 @@ package org.lib.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@Enable
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
